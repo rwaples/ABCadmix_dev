19aa95237e2c8c09b6055a6a796726c31ac4b954
ryan waples <ryan.waples@gmail.com>
2020-08-11 14:01:52 +0200

    allow user specified recombinations maps, allow saving of simulated tree-sequences

--- simulate.py
+++ simulate.py
@@ -1,22 +1,16 @@
-import scipy as sp
-import random
-import numpy as np
-import pandas as pd
-import itertools
-import collections
 import msprime
 import tskit
-from math import log
-from math import exp
 
-def sim_pulse(L = 1e9, Ne = 10000, Nadmix = 500,
+
+def sim_pulse(rec_map = None, L = 1e9, Ne = 10000, Nadmix = 500,
     Tadmix = 8, frac_p0 = 0.5,
-    seed = None, path = None):
+    seed = None, path = None, tszip = None):
     """
-    simulate a simple pulse model of admixture.
+    simulate a simple pulse model of admixture with the disrete-time backwards wright-fisher.
 
-
-    L = length of genome, in base pairs
+    rec_map = valid msprime recombination map
+    L = length of genome, in base pairs (ignored if rec_map is specified)
+    
     Ne = diploid population size for all three populations (2*source & admixed)
     Tadmix = time of admixture
     Nadmix = number of observed admixed individuals
@@ -27,12 +21,15 @@ def sim_pulse(L = 1e9, Ne = 10000, Nadmix = 500,
 
     # convert to correct dtypes and catch problems
     Tadmix = int(Tadmix)
-    L = int(L)
     Ne = int(Ne)
     Nadmix = int(Nadmix)
 
     # recombination map
-    recomb_map = msprime.RecombinationMap.uniform_map(L, 1e-8, L)
+    if rec_map:
+        recomb_map = rec_map
+    else:
+        L = int(L)
+        recomb_map = msprime.RecombinationMap.uniform_map(L, 1e-8, L)
 
 
     pop_configs = [
@@ -41,7 +38,7 @@ def sim_pulse(L = 1e9, Ne = 10000, Nadmix = 500,
         msprime.PopulationConfiguration(initial_size = Ne, growth_rate = 0)
         ]
 
-    # no ongoing migration.
+    # no ongoing migration
     mig_mat = [
         [0,0,0],
         [0,0,0],
@@ -49,9 +46,8 @@ def sim_pulse(L = 1e9, Ne = 10000, Nadmix = 500,
     ]
 
     admixture_events = [
-        # intial 50% fraction from each pop
         msprime.MassMigration(time=Tadmix, source = 2, destination = 0, proportion = frac_p0),
-        msprime.MassMigration(time=Tadmix + 1, source = 2, destination = 1,proportion = 1.0),
+        msprime.MassMigration(time=Tadmix + 1, source = 2, destination = 1, proportion = 1.0),
     ]
 
     Nsamp = int(Nadmix)
@@ -69,5 +65,15 @@ def sim_pulse(L = 1e9, Ne = 10000, Nadmix = 500,
         start_time = 0,
         end_time = Tadmix+2
     )
+    
+    if path:
+        if tszip:
+            # save compressed ts
+            import tszip
+            tszip.compress(ts_admix, path, variants_only = False)
+        else:
+            # save uncompressed ts
+            ts_admix.dump(path)
+            
 
     return(ts_admix)
