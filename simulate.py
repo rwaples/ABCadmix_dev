b9a9011b247abab993565e7ba147c45c456e6377
ryan waples <ryan.waples@gmail.com>
2020-08-14 16:02:22 +0200

    added two pulse model, standardize which populations are the sources of admixture across models

--- simulate.py
+++ simulate.py
@@ -44,8 +44,8 @@ def sim_pulse(rec_map=None, L=1e9, Ne=10000, Nadmix=500,
     ]
 
     admixture_events = [
-        msprime.MassMigration(time=Tadmix, source=2, destination=0, proportion=frac_p0),
-        msprime.MassMigration(time=Tadmix + 1, source=2, destination=1, proportion=1.0),
+        msprime.MassMigration(time=Tadmix, source=2, destination=1, proportion=frac_p0),
+        msprime.MassMigration(time=Tadmix + 1, source=2, destination=0, proportion=1.0),
     ]
 
     samps = [msprime.Sample(population=2, time=0)] * 2*Nadmix
@@ -61,6 +61,84 @@ def sim_pulse(rec_map=None, L=1e9, Ne=10000, Nadmix=500,
         random_seed=seed,
         start_time=0,
         end_time=Tadmix + 2
+    )
+
+    if path:
+        if tszip:
+            # save compressed ts
+            import tszip
+            tszip.compress(ts_admix, path, variants_only=False)
+        else:
+            # save uncompressed ts
+            ts_admix.dump(path)
+
+    return(ts_admix)
+
+
+def sim_two_pulse(rec_map=None, L=1e9, Ne=10000, Nadmix=500,
+                T1=4, T2 = 12, frac1=.2, frac2=.2,
+                seed=None, path=None, tszip=None):
+    """
+    simulate a simple pulse model of admixture
+    with the disrete-time backwards wright-fisher.
+
+    rec_map = valid msprime recombination map
+    L = length of genome, in base pairs (ignored if rec_map is specified)
+
+    Ne = diploid population size for all three populations
+    Tadmix = time of admixture
+    Nadmix = number of observed admixed diploid individuals
+    seed = seed passed to msprime.simulate()
+    path = file path, if given will write the ts to this path
+    """
+
+    assert T2>T1
+
+    # convert to correct dtypes and catch problems
+    T1 = int(T1)
+    T2 = int(T2)
+    Ne = int(Ne)
+    Nadmix = int(Nadmix)
+
+    # recombination map
+    if rec_map:
+        recomb_map = rec_map
+    else:
+        L = int(L)
+        recomb_map = msprime.RecombinationMap.uniform_map(L, 1e-8, L)
+
+    pop_configs = [
+        msprime.PopulationConfiguration(initial_size=Ne, growth_rate=0),
+        msprime.PopulationConfiguration(initial_size=Ne, growth_rate=0),
+        msprime.PopulationConfiguration(initial_size=Ne, growth_rate=0)
+        ]
+
+    # no ongoing migration
+    mig_mat = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+
+    admixture_events = [
+        msprime.MassMigration(time=T1, source=2, destination=1, proportion=frac1),
+        msprime.MassMigration(time=T2, source=2, destination=1, proportion=frac2),
+        msprime.MassMigration(time=T2 + 1, source=2, destination=0, proportion=1.0),
+    ]
+
+    samps = [msprime.Sample(population=2, time=0)] * 2*Nadmix
+
+    ts_admix = msprime.simulate(
+        population_configurations=pop_configs,
+        migration_matrix=mig_mat,
+        demographic_events=admixture_events,
+        recombination_map=recomb_map,
+        mutation_rate=0,
+        model='dtwf',
+        samples=samps,
+        random_seed=seed,
+        start_time=0,
+        end_time=T2 + 2
     )
 
     if path:
