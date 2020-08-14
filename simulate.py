13ca0b6b0e7e50ba99f1adf492cf191f6308e4de
ryan waples <ryan.waples@gmail.com>
2020-08-14 15:33:28 +0200

    added ongoing admixture models

--- simulate.py
+++ simulate.py
@@ -11,11 +11,11 @@ def sim_pulse(rec_map=None, L=1e9, Ne=10000, Nadmix=500,
     rec_map = valid msprime recombination map
     L = length of genome, in base pairs (ignored if rec_map is specified)
 
-    Ne = diploid population size for all three populations (2*source & admixed)
+    Ne = diploid population size for all three populations
     Tadmix = time of admixture
-    Nadmix = number of observed admixed individuals
-    seed = seed to pass to msprime.simulate
-    path = file path, if given will write the ts to this path (NOT IMPLEMENTED)
+    Nadmix = number of observed admixed diploid individuals
+    seed = seed passed to msprime.simulate()
+    path = file path, if given will write the ts to this path
     """
 
     # convert to correct dtypes and catch problems
@@ -48,8 +48,7 @@ def sim_pulse(rec_map=None, L=1e9, Ne=10000, Nadmix=500,
         msprime.MassMigration(time=Tadmix + 1, source=2, destination=1, proportion=1.0),
     ]
 
-    Nsamp = int(Nadmix)
-    samps = [msprime.Sample(population=2, time=0)] * Nsamp
+    samps = [msprime.Sample(population=2, time=0)] * 2*Nadmix
 
     ts_admix = msprime.simulate(
         population_configurations=pop_configs,
@@ -67,10 +66,153 @@ def sim_pulse(rec_map=None, L=1e9, Ne=10000, Nadmix=500,
     if path:
         if tszip:
             # save compressed ts
-            import tszip # 
+            import tszip
             tszip.compress(ts_admix, path, variants_only=False)
         else:
             # save uncompressed ts
             ts_admix.dump(path)
+
+    return(ts_admix)
+
+
+def sim_ongoing_constant(rec_map=None, L=3e9, Ne=10000, Nadmix=500,
+                            Tadmix=8, frac_ongoing=0.05,
+                            seed=None, path=None, tszip=None):
+
+    """
+    simulate an ongoing model of admixture
+    with the disrete-time backwards wright-fisher.
+
+    A new population (2) is formed by splitting off from population 0.
+    At time=Tadmix migration starts from population 1, with rate frac_ongoing
+    admixture continues until the present (time of sampling)
+
+    rec_map = valid msprime recombination map
+    L = length of genome, in base pairs (ignored if rec_map is specified)
+
+    Ne = diploid population size for all three populations (2*source & admixed)
+    Tadmix = time of admixture
+    Nadmix = number of observed admixed individuals
+    seed = seed to pass to msprime.simulate
+    path = file path, if given will write the ts to this path (NOT IMPLEMENTED)
+    """
+
+    Tadmix = int(Tadmix)
+    Ne = int(Ne)
+    Nadmix = int(Nadmix)
+
+    # recombination map
+    if rec_map:
+        recomb_map = rec_map
+    else:
+        L = int(L)
+        recomb_map = msprime.RecombinationMap.uniform_map(L, 1e-8, L)
+
+    pop_configs = [
+        msprime.PopulationConfiguration(initial_size=Ne, growth_rate=0),
+        msprime.PopulationConfiguration(initial_size=Ne, growth_rate=0),
+        msprime.PopulationConfiguration(initial_size=Ne, growth_rate=0)
+        ]
+
+    mig_mat = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, frac_ongoing, 0],
+    ]
+
+    admixture_events = [
+        # set migration rates back to zero with the admixed population
+        msprime.MigrationRateChange(time=Tadmix, rate=0, matrix_index=(2, 1)),
+        msprime.MassMigration(time=Tadmix+1, source=2, destination=0, proportion=1.0),
+    ]
+
+    samps = [msprime.Sample(population=2, time=0)] * 2*Nadmix
+
+    ts_admix = msprime.simulate(
+        population_configurations=pop_configs,
+        migration_matrix=mig_mat,
+        demographic_events=admixture_events,
+        recombination_map=recomb_map,
+        mutation_rate=0,
+        model='dtwf',
+        samples=samps,
+        random_seed=seed,
+        start_time=0,
+        end_time=Tadmix + 2
+    )
+
+    return(ts_admix)
+
+
+def sim_ongoing_interval(rec_map=None, L=3e9, Ne=10000, Nadmix=500,
+                Tadmix_start=4, Tadmix_stop=12, frac_ongoing=0.05,
+                seed=None, path=None, tszip=None):
+
+    """
+    Simulate an ongoing model of admixture
+    with the disrete-time backwards wright-fisher.
+
+    A new population (2) is formed by splitting off from population 0.
+    At time=Tadmix_start migration starts from population 1,
+    with rate frac_ongoing admixture continues until Tadmix_stop.
+
+    rec_map = valid msprime recombination map
+    L = length of genome, in base pairs (ignored if rec_map is specified)
+
+    Ne = diploid population size for all three populations (2*source & admixed)
+    Tadmix = time of admixture
+    Nadmix = number of observed admixed individuals
+    seed = seed to pass to msprime.simulate
+    path = file path, if given will write the ts to this path (NOT IMPLEMENTED)
+    """
+
+    assert Tadmix_stop > Tadmix_start
+
+    Tadmix_start = int(Tadmix_start)
+    Tadmix_stop = int(Tadmix_stop)
+    Ne = int(Ne)
+    Nadmix = int(Nadmix)
+
+    # recombination map
+    if rec_map:
+        recomb_map = rec_map
+    else:
+        L = int(L)
+        recomb_map = msprime.RecombinationMap.uniform_map(L, 1e-8, L)
+
+    pop_configs = [
+        msprime.PopulationConfiguration(initial_size=Ne, growth_rate=0),
+        msprime.PopulationConfiguration(initial_size=Ne, growth_rate=0),
+        msprime.PopulationConfiguration(initial_size=Ne, growth_rate=0)
+        ]
+
+    mig_mat = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+
+    admixture_events = [
+        # migration during the interval Tadmix_start - Tadmix_stop
+        msprime.MigrationRateChange(time=Tadmix_start, rate=frac_ongoing, matrix_index=(2, 1)),
+        msprime.MigrationRateChange(time=Tadmix_stop, rate=0, matrix_index=(2, 1)),
+        # founding of pop 2
+        msprime.MassMigration(time=Tadmix_stop + 1, source=2, destination=0, proportion=1.0),
+    ]
+
+    samps = [msprime.Sample(population=2, time=0)] * 2*Nadmix
+
+    ts_admix = msprime.simulate(
+        population_configurations=pop_configs,
+        migration_matrix=mig_mat,
+        demographic_events=admixture_events,
+        recombination_map=recomb_map,
+        mutation_rate=0,
+        model='dtwf',
+        samples=samps,
+        random_seed=seed,
+        start_time=0,
+        end_time=Tadmix_stop + 2
+    )
 
     return(ts_admix)
